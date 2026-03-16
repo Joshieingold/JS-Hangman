5ff08c2cd04b9c0fbb89db855e106bd6be35588f
josh <joshlynch2001@gmail.com>
2026-03-16 20:09:27 -0300

    all cleaned up and working!

--- hangman.js
+++ hangman.js
@@ -1,231 +1,31 @@
-let wrongCounter = 0;
-let secretWord;
-let foundWord;
+///////////////
+// Constants //
+///////////////
+
+const guessAmount = 6;
+
+////////////
+// Global //
+////////////
+
 let jsonData;
+let remainingGuesses = guessAmount;
+let hiddenWord;
+let userWord;
+
 window.onload = function () {
     RequestJsonData();
-    ShowStartScreen();
-};
-function InsertCounter() {
-    target = document.querySelector("#counter");
-    target.innerHTML = `Guesses Remaining: ${6 - wrongCounter}`;
-}
-function ShowStartScreen() {
-    document.querySelector("#start-container").classList.remove("hidden");
-    document.querySelector("#result-container").classList.add("hidden");
-    document.querySelector("#game-container").classList.add("hidden");
+    GoToSelectionScreen();
     document
         .querySelector("#start-button")
-        .addEventListener("click", ShowStartGame);
-}
-function GetRandomWord() {
-    let chosenCategory = document.querySelector("#category-selector").value;
-    let vocabs = jsonData.vocabularies;
-    for (let i = 0; i < vocabs.length; i++) {
-        if (vocabs[i].categoryName === chosenCategory) {
-            let randomIndex = GetRandomIndexInRange(vocabs[i].words.length);
-            secretWord = vocabs[i].words[randomIndex];
-        }
-    }
-}
-function GetRandomIndexInRange(max) {
-    let randomIndex = Math.floor(Math.random() * max);
-    return randomIndex;
-}
-function ShowResultScreen() {
-    document.querySelector("#start-container").classList.add("hidden");
-    document.querySelector("#result-container").classList.remove("hidden");
-    document.querySelector("#game-container").classList.remove("hidden");
+        .addEventListener("click", GoToGameScreen);
     document
         .querySelector("#play-again-button")
         .addEventListener("click", RestartGame);
-}
-function RestartGame() {
-    let allLetters = document.querySelectorAll(".used");
-    for (let i = 0; i < allLetters.length; i++) {
-        allLetters[i].classList.remove("used");
-        allLetters[i].classList.remove("correct");
-        allLetters[i].classList.remove("incorrect");
-    }
-    wrongCounter = 0;
-    ShowStartScreen();
-}
-function ClearHangman() {
-    const c = document.querySelector("#hangman-canvas");
-    const ctx = c.getContext("2d");
-    ctx.clearRect(0, 0, c.width, c.height);
-}
-function ShowStartGame(wordToUse) {
-    document.querySelector("#start-container").classList.add("hidden");
-    document.querySelector("#result-container").classList.add("hidden");
-    document.querySelector("#game-container").classList.remove("hidden");
-    GetRandomWord();
-    ClearHangman();
-    CreateGallows();
-    foundWord = CreateBlankFoundWord();
-    CreateFoundWordHTML();
-    InsertCounter();
-    let letterButtons = document.querySelector("#letter-buttons");
-    letterButtons.addEventListener("click", LetterClick);
-}
-
-function CreateBlankFoundWord() {
-    returnString = "";
-    for (let i = 0; i < secretWord.length; i++) {
-        returnString += "*";
-    }
-    return returnString;
-}
-function CreateFoundWordHTML() {
-    let targetLocation = document.querySelector("#found-container");
-    let html = "";
-    for (let i = 0; i < foundWord.length; i++) {
-        if (foundWord[i] !== "*") {
-            html += "<span class='found-letter correct'>";
-        } else {
-            html += "<span class='found-letter'>";
-        }
-        html += foundWord[i];
-        html += "</span>";
-    }
-    targetLocation.innerHTML = html;
-}
-function StartHangingHim() {
-    if (wrongCounter === 0) {
-        DrawHead();
-        wrongCounter++;
-    } else if (wrongCounter === 1) {
-        DrawBody();
-        wrongCounter++;
-    } else if (wrongCounter === 2) {
-        DrawLeftArm();
-        wrongCounter++;
-    } else if (wrongCounter === 3) {
-        DrawRightArm();
-        wrongCounter++;
-    } else if (wrongCounter === 4) {
-        DrawLeftLeg();
-        wrongCounter++;
-    } else if (wrongCounter === 5) {
-        DrawRightLeg();
-        wrongCounter++;
-    }
-    InsertCounter();
-}
-function LetterClick(e) {
-    let letter = e.target.innerHTML;
-    if (letter.length > 1) {
-        return;
-    }
-    if (e.target.classList.contains("used")) {
-        return;
-    }
-    e.target.classList.add("used");
-    if (secretWord.toLowerCase().includes(letter.toLowerCase())) {
-        CheckLetter(letter.toLowerCase());
-        e.target.classList.add("correct");
-    } else {
-        StartHangingHim();
-        e.target.classList.add("incorrect");
-    }
-    CreateFoundWordHTML();
-    if (CheckWin()) {
-        CreateWinHTML();
-        ShowResultScreen();
-    }
-    if (CheckLoss()) {
-        CreateLossHTML();
-        ShowResultScreen();
-    }
-}
-function CheckLoss() {
-    if (wrongCounter >= 6) {
-        return true;
-    }
-    return false;
-}
-function CreateLossHTML() {
-    let targetRef = document.querySelector("#result-message-container");
-
-    let html = "";
-    html += "<h2 class='incorrect-text'>You lost!</h2>";
-    html += `<p>The word was ${secretWord}</p>`;
-
-    targetRef.innerHTML = html;
-}
-function CreateWinHTML() {
-    let targetRef = document.querySelector("#result-message-container");
-    let html = "";
-    html += "<h2 class='correct-text'>You Win!!</h2>";
-    html += `<p>The word was ${secretWord}</p>`;
-    targetRef.innerHTML = html;
-}
-function CheckWin() {
-    if (foundWord === secretWord) {
-        return true;
-    }
-    return false;
-}
-function CheckLetter(letter) {
-    let returnWord = foundWord;
-    let newString = "";
-    for (let i = 0; i < secretWord.length; i++) {
-        if (secretWord[i] === letter) {
-            newString += letter;
-        } else {
-            newString += returnWord[i];
-        }
-    }
-    foundWord = newString;
-}
-
-function DrawHead() {
-    let c = document.querySelector("#hangman-canvas");
-    let ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(350, 240, 40, 0, 2 * Math.PI);
-    ctx.stroke();
-}
-function DrawBody() {
-    let c = document.querySelector("#hangman-canvas");
-    let ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.moveTo(350, 280);
-    ctx.lineTo(350, 370);
-    ctx.stroke();
-}
-function DrawLeftArm() {
-    let c = document.querySelector("#hangman-canvas");
-    let ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.moveTo(350, 280);
-    ctx.lineTo(320, 360);
-    ctx.stroke();
-}
-function DrawRightArm() {
-    let c = document.querySelector("#hangman-canvas");
-    let ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.moveTo(350, 280);
-    ctx.lineTo(380, 360);
-    ctx.stroke();
-}
-function DrawLeftLeg() {
-    let c = document.querySelector("#hangman-canvas");
-    let ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.moveTo(350, 365);
-    ctx.lineTo(330, 420);
-    ctx.stroke();
-}
-function DrawRightLeg() {
-    let c = document.querySelector("#hangman-canvas");
-    let ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.moveTo(350, 365);
-    ctx.lineTo(370, 420);
-    ctx.stroke();
-}
+    document
+        .querySelector("#letter-buttons")
+        .addEventListener("click", LetterClick);
+};
 
 function RequestJsonData() {
     let url = "./data/vocabularies.json";
@@ -234,16 +34,44 @@ function RequestJsonData() {
         if (this.readyState == 4 && this.status == 200) {
             data = JSON.parse(xhr.responseText);
             jsonData = data;
-            CreateDropDown();
+            SetDropDownValues(CreateDropDownHTML());
         }
     };
     xhr.open("GET", url, true);
     xhr.send();
 }
 
-function CreateGallows() {
-    let c = document.querySelector("#hangman-canvas");
-    let ctx = c.getContext("2d");
+/////////////////////
+// Screen Handling //
+/////////////////////
+
+function GoToSelectionScreen() {
+    document.querySelector("#start-container").classList.remove("hidden");
+    document.querySelector("#result-container").classList.add("hidden");
+    document.querySelector("#game-container").classList.add("hidden");
+}
+function GoToGameScreen() {
+    RefreshGame();
+    document.querySelector("#start-container").classList.add("hidden");
+    document.querySelector("#result-container").classList.add("hidden");
+    document.querySelector("#game-container").classList.remove("hidden");
+}
+function GoToResultScreen() {
+    document.querySelector("#start-container").classList.add("hidden");
+    document.querySelector("#result-container").classList.remove("hidden");
+    document.querySelector("#game-container").classList.remove("hidden");
+}
+
+////////////////////
+// Canvas Drawing //
+////////////////////
+
+function RefreshCanvas() {
+    // Clear
+    const c = document.querySelector("#hangman-canvas");
+    const ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, c.width, c.height);
+    // Draw Gallow
     ctx.beginPath();
     ctx.moveTo(100, 100);
     ctx.lineTo(350, 100);
@@ -259,12 +87,237 @@ function CreateGallows() {
     ctx.stroke();
 }
 
-function CreateDropDown() {
-    let selectorRef = document.querySelector("#category-selector");
+function DrawHead() {
+    let c = document.querySelector("#hangman-canvas");
+    let ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(350, 240, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+function DrawBody() {
+    let c = document.querySelector("#hangman-canvas");
+    let ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(350, 280);
+    ctx.lineTo(350, 370);
+    ctx.stroke();
+}
+
+function DrawLeftArm() {
+    let c = document.querySelector("#hangman-canvas");
+    let ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(350, 280);
+    ctx.lineTo(320, 360);
+    ctx.stroke();
+}
+
+function DrawRightArm() {
+    let c = document.querySelector("#hangman-canvas");
+    let ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(350, 280);
+    ctx.lineTo(380, 360);
+    ctx.stroke();
+}
+
+function DrawLeftLeg() {
+    let c = document.querySelector("#hangman-canvas");
+    let ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(350, 365);
+    ctx.lineTo(330, 420);
+    ctx.stroke();
+}
+
+function DrawRightLeg() {
+    let c = document.querySelector("#hangman-canvas");
+    let ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(350, 365);
+    ctx.lineTo(370, 420);
+    ctx.stroke();
+}
+
+////////////////
+// Game Logic //
+////////////////
+
+function RestartGame() {
+    RefreshGame();
+    GoToSelectionScreen();
+}
+
+function LetterClick(e) {
+    let letter = e.target.innerHTML;
+    if (letter.length > 1) {
+        return;
+    }
+    if (e.target.classList.contains("used")) {
+        return;
+    }
+    e.target.classList.add("used");
+    if (hiddenWord.toLowerCase().includes(letter.toLowerCase())) {
+        UpdateGuess(letter.toLowerCase());
+        e.target.classList.add("correct");
+    } else {
+        HandleIncorrect();
+        e.target.classList.add("incorrect");
+    }
+    UpdatePlayerWordValue();
+    if (CheckWin()) {
+        SetResultPopOver("win");
+        GoToResultScreen();
+    }
+    if (CheckLose()) {
+        SetResultPopOver("lose");
+        GoToResultScreen();
+    }
+}
+
+function CheckWin() {
+    return hiddenWord === userWord;
+}
+
+function CheckLose() {
+    return remainingGuesses <= 0;
+}
+function UpdateGuess(letter) {
+    let rString = "";
+    for (let i = 0; i < hiddenWord.length; i++) {
+        if (hiddenWord[i] === letter) {
+            rString += letter;
+        } else {
+            rString += userWord[i];
+        }
+    }
+    userWord = rString;
+}
+function HandleIncorrect() {
+    if (remainingGuesses === 6) {
+        DrawHead();
+    } else if (remainingGuesses === 5) {
+        DrawBody();
+    } else if (remainingGuesses === 4) {
+        DrawLeftArm();
+    } else if (remainingGuesses === 3) {
+        DrawRightArm();
+    } else if (remainingGuesses === 2) {
+        DrawLeftLeg();
+    } else if (remainingGuesses === 1) {
+        DrawRightLeg();
+    }
+    remainingGuesses--;
+    UpdateGuessCounter();
+}
+
+//////////////////////
+// Helper Functions //
+//////////////////////
+
+function SetRandomWord() {
+    let chosenCategory = document.querySelector("#category-selector").value;
+    let vocab = jsonData.vocabularies;
+    for (let i = 0; i < vocab.length; i++) {
+        if (vocab[i].categoryName === chosenCategory) {
+            let randomIndex = GetRandomIndexInRange(vocab[i].words.length);
+            hiddenWord = vocab[i].words[randomIndex];
+        }
+    }
+}
+
+function GetRandomIndexInRange(max) {
+    let randomIndex = Math.floor(Math.random() * max);
+    return randomIndex;
+}
+
+function RefreshGame() {
+    // Clear HTML classes
+    let dirtyLetters = document.querySelectorAll(".used");
+    for (let i = 0; i < dirtyLetters.length; i++) {
+        currentLetter = dirtyLetters[i];
+        currentLetter.classList.remove("used");
+        currentLetter.classList.remove("correct");
+        currentLetter.classList.remove("incorrect");
+    }
+
+    // Refresh states
+    remainingGuesses = guessAmount;
+    SetRandomWord();
+    userWord = GetCensoredWord(hiddenWord.length);
+
+    // Update UI
+    RefreshCanvas();
+    UpdatePlayerWordValue();
+    UpdateGuessCounter();
+}
+
+function GetCensoredWord(length) {
+    let rString = "";
+    for (let i = 0; i < length; i++) {
+        rString += "*";
+    }
+    return rString;
+}
+
+//////////////////////
+// DOM Manipulation //
+//////////////////////
+
+function SetDropDownValues(html) {
+    let categorySelectorRef = document.querySelector("#category-selector");
+    categorySelectorRef.innerHTML = html;
+}
+function SetResultPopOver(result) {
+    let resultPopOver = document.querySelector("#result-message-container");
+    resultPopOver.innerHTML = CreateResultHTML(result);
+}
+function UpdateGuessCounter() {
+    counter = document.querySelector("#counter");
+    counter.innerHTML = CreateCounterHTML();
+}
+function UpdatePlayerWordValue() {
+    let letterSpans = document.querySelector("#found-container");
+    letterSpans.innerHTML = CreatePlayerWordHTML();
+}
+
+///////////////////
+// HTML Creation //
+///////////////////
+
+function CreatePlayerWordHTML() {
+    let html = "";
+    for (let i = 0; i < userWord.length; i++) {
+        if (userWord[i] !== "*") {
+            html += "<span class='found-letter correct'>";
+        } else {
+            html += "<span class='found-letter'>";
+        }
+        html += userWord[i];
+        html += "</span>";
+    }
+    return html;
+}
+
+function CreateCounterHTML() {
+    return `Guesses Remaining: ${remainingGuesses}`;
+}
+function CreateResultHTML(result) {
+    let html = "";
+    if (result === "lose") {
+        html += "<h2 class='incorrect-text'>You lost!</h2>";
+    } else {
+        html += "<h2 class='correct-text'>You Win!!</h2>";
+    }
+    html += `<p>The word was ${hiddenWord}</p>`;
+    return html;
+}
+function CreateDropDownHTML() {
     let html = "";
     let vocab = jsonData.vocabularies;
     for (let i = 0; i < vocab.length; i++) {
         html += `<option value='${vocab[i].categoryName}'>${vocab[i].categoryName}</option>`;
     }
-    selectorRef.innerHTML = html;
+    return html;
 }
